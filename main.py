96cc6aae3eb5cc67f4602754d43470560c6f1019
Asif Ahmed <iamasif93@gmail.com>
2023-10-10 15:00:39 +0600

    auto-birthday-mailer v1.1

--- main.py
+++ main.py
@@ -1,12 +1,3 @@
-##################### Extra Hard Starting Project ######################
-
-# 1. Update the birthdays.csv
-
-# 2. Check if today matches a birthday in the birthdays.csv
-
-# 3. If step 2 is true, pick a random letter from letter templates and replace the [NAME] with the person's actual name from birthdays.csv
-
-# 4. Send the letter generated in step 3 to that person's email address.
 
 import smtplib
 import random
@@ -57,8 +48,3 @@ for item in birthdays_today:
         connection.login(user=my_email, password=password)
         connection.sendmail(from_addr=my_email,
                             to_addrs=receiver_email, msg=f"Subject:Happy Birthday\n\nDear {receiver_name},\n{message}")
-
-
-
-
-
